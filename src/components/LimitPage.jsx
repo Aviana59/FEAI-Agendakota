eae1b88b967e656dffb509608b22677b9b26c21a
22091397059 Aviana Candra <aviana.22059@mhs.unesa.ac.id>
2025-02-01 21:27:18 +0700

    Limit & Success

--- src/components/LimitPage.jsx
+++ src/components/LimitPage.jsx
@@ -1,9 +1,22 @@
-const LimitPage = () => {
+import LIMIT_ICON from "@/assets/Limit.svg"
+import { NavLink } from "react-router-dom";
+const SuccessPage = () => {
     return (
         <>
-            <p>Limit!</p>
+            <div className="flex flex-col justify-between mt-52 items-center h-screen">
+                <div className="flex flex-col gap-14 items-center">
+                    <div className="">
+                        <img src={LIMIT_ICON} alt="" width="600" />
+                    </div>
+                    <div className="">
+                        <p className="font-semibold text-3xl">Halo! Anda telah menggunakan semua kesempatan yang tersedia untuk layanan AI kami dalam membantu Anda.
+                            Terima kasih telah memanfaatkan layanan kami</p>
+                    </div>
+                </div>
+                <NavLink to="/" className="w-full p-4 bg-primary rounded-md text-center font-bold text-white text-lg">Home</NavLink>
+            </div>
         </>
     )
 }
 
-export default LimitPage;
+export default SuccessPage;
--- src/components/SuccessPage.jsx
+++ src/components/SuccessPage.jsx
@@ -1,21 +1,29 @@
-import SUCCESS_ICON from "@/assets/success-1.svg"
+import SUCCESS_ICON from "@/assets/success-1.svg";
 import { NavLink } from "react-router-dom";
+
 const SuccessPage = () => {
     return (
         <>
             <div className="flex flex-col justify-between mt-52 items-center h-screen">
                 <div className="flex flex-col gap-14 items-center">
-                    <div className="">
-                        <img src={SUCCESS_ICON} alt="" width="600"/>
+                    <div>
+                        <img src={SUCCESS_ICON} alt="Success Icon" width="600" />
                     </div>
-                    <div className="">
-                        <p className="font-semibold text-5xl">Terimakasih dan silahkan cek Email kamu ya.</p>
+                    <div>
+                        <p className="font-semibold text-5xl">
+                            Terimakasih dan silahkan cek Email kamu ya.
+                        </p>
                     </div>
                 </div>
-                <NavLink to="/" className="w-full p-4 bg-primary rounded-md text-center font-bold text-white text-lg">Home</NavLink>
+                <NavLink 
+                    to="/" 
+                    className="w-full p-4 bg-primary rounded-md text-center font-bold text-white text-lg"
+                >
+                    Home
+                </NavLink>
             </div>
         </>
-    )
-}
+    );
+};
 
 export default SuccessPage;
